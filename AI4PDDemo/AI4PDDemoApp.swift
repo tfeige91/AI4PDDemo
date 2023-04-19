58207c592c8f203c25b2663132c21bc897eae6c0
Timfeige <feige_tim@yahoo.de>
2023-04-19 14:13:44 +0200

    Initial Commit

---
+++ AI4PDDemo/AI4PDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI4PDDemoApp.swift
+//  AI4PDDemo
+//
+//  Created by Tim_Feige on 19.04.23.
+//
+
+import SwiftUI
+
+@main
+struct AI4PDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
